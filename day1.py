443ae928f08ee07af6c8bd715d77cad244f4948a
israhadi88 <israhadithl@gmail.com>
2026-04-13 16:02:42 +0700

    initial commit python

---
+++ day1.py
@@ -0,0 +1,29 @@
+print ('Hello world')
+print ('Hello world2')
+
+
+#python identatation
+if 5 > 2:
+    print ('true')
+if 4 > 2:
+         print ('true')
+
+if 5 > 2:
+    print("Five is greater than two!") 
+    print("Five is greater than two!")
+
+#python variable
+x = 2
+y = 'Horray'
+
+print (x,y)
+
+#python statement
+print("ini adalah statement")
+print("ini adalah statement ke 2")
+
+#print statement without new line
+print("ini adalah statement", end="")
+print(" ini adalah statement ke 2")
+
+#
