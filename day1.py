eaefa2f677488c81bcea62867486b985b2e1b3e3
israhadi88 <israhadithl@gmail.com>
2026-04-13 16:31:46 +0700

    add: data type in python

--- day1.py
+++ day1.py
@@ -26,4 +26,53 @@ print("ini adalah statement ke 2")
 print("ini adalah statement", end="")
 print(" ini adalah statement ke 2")
 
-#
+#daya type in python
+#1. integer
+my_integer_var = 10
+print('Integer:', my_integer_var)
+print(type(my_integer_var))
+
+#2. string
+my_string_var = 'String'
+print('String:',my_string_var)
+print(type(my_string_var))
+
+#3. float
+my_float_var = 4.50
+print('Float:', my_float_var)
+print(type(my_float_var))
+
+#4. boolean
+my_boolean_var = True
+print('Boolean:',my_boolean_var)
+print(type(my_boolean_var))
+
+#5. set (an unordered collection, must be unique element, set cant be indexing)
+my_set_var = {7, 'Hello', 4.5}
+print('Set:',my_set_var)
+print(type(my_set_var))
+
+#6. dictionary (an ordered collection of key-value pairs)
+my_dictionary_var = {'name': 'John', 'Age': 29}
+print('Dictionary:', my_dictionary_var)
+print(type(my_dictionary_var))
+
+#7. Tuple (an ordered immutable collection)
+my_tuple_var = (7, 'Hello',4.5)
+print('Tupple:', my_tuple_var)
+print(type(my_tuple_var))
+
+#8. LIst (an ordered, mutable, can indexing)
+my_list_var = [2,'israhadi',4.6]
+print('List:', my_list_var)
+print(type(my_list_var))
+
+#9. Range (A sequence of number, often used in loops)
+my_range_var = range(5)
+print('Range:', my_range_var)
+print(type(my_range_var))
+
+#10. None (Use to represent the absence of value)
+my_none_var = None
+print('None:', my_none_var)
+print(type(my_none_var))
