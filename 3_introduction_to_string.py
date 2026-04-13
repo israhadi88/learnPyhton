d3318a8a52939a44d0e8c225325103bbbf3ad3e6
israhadi88 <israhadithl@gmail.com>
2026-04-13 23:12:37 +0700

    masuk ke lesson 4

--- 3_introduction_to_string.py
+++ 3_introduction_to_string.py
@@ -73,3 +73,40 @@ age = 29
 
 name_age = f'my name is: {name} and i am {age} years old'
 print(name_age)
+
+#LESSON 3 Slicing 
+#access string with specific part of it 
+#string[start:stop]
+
+#example
+slc = 'israhadi'
+print(slc[1:3])
+
+#or start to selected index
+slc2 = 'israhadi'
+print(slc2[:4])
+
+#start from selected index to end
+slc3 = 'israhadi'
+print(slc3[4:])
+
+#we can add some step
+slc4 = 'israhadi'
+print(slc4[0:9:2])
+
+#or step backward
+slc4 = 'israhadi'
+print(slc4[::-1])
+
+#LESSON 4 COMMON STRING METHOD
+cmmn = 'Hello World'
+print(cmmn.lower()) #to lowercase
+print(cmmn.upper()) #to uppercase
+
+cmmn2 = 'Hello World'
+print(cmmn2.strip()) #make into single word space
+print(cmmn2.replace('Hello','hi')) #replace string to 
+print(cmmn2.split()) #split into list
+
+cmmn3 = cmmn2.split()
+print(' '.join(cmmn3)) #list (immutable into string)
