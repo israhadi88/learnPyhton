b9282bf5a997cc9f5296da3bd3ef1a763538f6ab
israhadi88 <israhadithl@gmail.com>
2026-04-17 16:18:51 +0700

    update

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,6 +1,6 @@
 running_total = 0
 
-num_of_friends = int(input('Input your number of person:'))
+num_of_friends = input('Input your number of person:')
 
 appetizers = int(input('Input your appetizers prize:'))
 main_courses = int(input('Input your main_courese prize:'))
@@ -11,11 +11,13 @@ running_total += appetizers + main_courses + desserts + drinks
 print('Total bill so far:', running_total)
 
 tip = running_total * 0.25
+print('Tip amount:', tip)
 
 running_total += tip
 print('Total with tip:', running_total)
 
 final_bill = running_total / num_of_friends
+print('Bill per person:', final_bill)
 
 each_pays = round(final_bill,2)
 print (f'Each person pays: {each_pays}')
