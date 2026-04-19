d9fc73abc06db69ee667d52e624b260de2c7ef16
israhadi88 <israhadithl@gmail.com>
2026-04-19 21:18:14 +0700

    update RPG

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,6 +1,9 @@
+#Build an RPG Character
+# In this lab you will practice the basics of Python by building a small app that 
+# creates a character for an RPG adventure.
+# Objective: Fulfill the user stories below and get all the tests to pass to complete the lab.
 full_dot = '●'
 empty_dot = '○'
-
 def create_character(name,strength, intelligence,charisma):
     if not isinstance(name,str):
         return 'The character name should be a string'
@@ -11,14 +14,16 @@ def create_character(name,strength, intelligence,charisma):
     elif ' ' in name:
         return 'The character name should not contain spaces'
     elif not isinstance(strength, int) or not isinstance(intelligence, int) or not isinstance(charisma, int):
-        return 'All stats should be integers'
+        return 'All stats should be intergers'
     elif strength < 1 or intelligence < 1 or charisma < 1:
         return 'All stats should be no less than 1'
     elif strength > 4 or intelligence > 4 or charisma > 4:
-        return 'All stats should be no more than 4'
+        return 'All stats should be no nome than 4'
     elif strength+intelligence+charisma != 7:
         return 'The character should start with 7 points'
     def stat_bar(value):
-        return full_dot * value + empty_dot * (10-value)
+        return full_dot * value + empty_dot * (10 - value)
+    
+    # Return formatted string
     return f"{name}\nSTR {stat_bar(strength)}\nINT {stat_bar(intelligence)}\nCHA {stat_bar(charisma)}"
-print(create_character('ren',4,2,1))
+print(create_character('ren',3,3,1))
