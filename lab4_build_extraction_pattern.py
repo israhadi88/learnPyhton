aabfc3ef7d96f70e7140b3d515ea88573a5f51b1
israhadi88 <israhadithl@gmail.com>
2026-05-02 15:56:18 +0700

    update

---
+++ lab4_build_extraction_pattern.py
@@ -0,0 +1,16 @@
+def number_pattern(n):
+    if not isinstance (n,int):
+            return 'Agument must be an integer value.'
+            return
+    if n < 1:
+            return ('Argument must be an integer greater than 0.')
+    numbers = []
+    for num in range(1, n + 1):
+        numbers.append(str(num))
+    
+    format =' '.join(numbers)
+    return format
+print(number_pattern(4))
+print(number_pattern(12))
+print(number_pattern('a'))
+print(number_pattern(0))
