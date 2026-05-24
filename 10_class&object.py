5cbdec2660037ddc4e6a4fd0df6bb77f39b79f32
israhadi88 <israhadithl@gmail.com>
2026-05-24 17:40:45 +0700

    update

--- 10_class&object.py
+++ 10_class&object.py
@@ -1,11 +1,12 @@
 class Dog:
-    def __init__(self,name, age):
+    def __init__(self, name, age):
         self.name = name
         self.age = age
 
     def bark(self):
         print(f"{self.name.upper()}! says woof woof! I'm {self.age} years old")
 
-dog_1 = Dog("Jack",3)
+
+dog_1 = Dog("Jack", 3)
 
 dog_1.bark()
