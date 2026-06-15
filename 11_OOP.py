df50b6a072c722c0284f6ab0df8b4758572c4168
israhadi88 <israhadithl@gmail.com>
2026-06-15 15:16:29 +0700

    oop

--- 11_OOP.py
+++ 11_OOP.py
@@ -1,15 +1,16 @@
 class Wallet:
-        def __init__(self,balance):
-            self._balance = balance
-            print(f"Dompet dibuat dengan saldo awal {self._balance}")
+    def __init__(self, balance):
+        self._balance = balance
+        print(f"Dompet dibuat dengan saldo awal {self._balance}")
 
-        
-        def deposit(self,amount):
-            print(f"Mencoba deposit: {amount}")
-            if amount > 0:
-                self._balance += amount
-                print(f"Berhasil menambahkan deposit {self._balance}")
-        
-        def withdraw(self,amount):
-            if 0 < amount <= self._balance:
-                self._balance -= amount
+    def deposit(self, amount):
+        print(f"Mencoba deposit: {amount}")
+        if amount > 0:
+            self._balance += amount
+            print(f"Berhasil menambahkan deposit {self._balance}")
+        else:
+            print(f"Transaksi gagal! angka tidak boleh 0 ")
+
+    def withdraw(self, amount):
+        if 0 < amount <= self._balance:
+            self._balance -= amount
