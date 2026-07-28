15f53cd54fd55e578bd7f02865e5367865857f50
israhadi88 <israhadithl@gmail.com>
2026-07-28 17:07:11 +0700

    27 Jul 2026, progress workshop selesai update

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,34 +1,65 @@
-class Circle:
-    def __init__(self, radius):
-        self.radius = radius
+class Employee:
+    _base_salaries = {
+        'trainee': 1000,
+        'junior': 2000,
+        'mid-level': 3000,
+        'senior': 4000,
+    }
 
-    # Getter
+    def __init__(self, name, level):
+        self.name = name
+        self.level = level
+        self.salary = Employee._base_salaries[level]
+
+    def __str__(self):
+        return f'{self.name}: {self.level}'
+
+    def __repr__(self):
+        return f"Employee('{self.name}', '{self.level}')"
+
     @property
-    def radius(self):
-        return self._radius
+    def name(self):
+        return self._name
 
-    # Setter
-    @radius.setter
-    def radius(self, value):
-        if value <= 0:
-            raise ValueError("Radius must be positive")
-        self._radius = value
+    @name.setter
+    def name(self, new_name):
+        if not isinstance(new_name, str):
+            raise TypeError("'name' must be a string.")
+        self._name = new_name
+        print(f"'name' updated to '{self.name}'.")
 
-    # Deleter
-    @radius.deleter
-    def radius(self):
-        print("Deleting radius...")
-        del self._radius
-my_circle = Circle(33)
-print("Initial radius:", my_circle.radius)  # 33
+    @property
+    def level(self):
+        return self._level
 
-# Delete the radius
-# This calls the deleter
-del my_circle.radius # Deleting radius...
-print("Radius deleted!") # Radius deleted!
+    @level.setter
+    def level(self, new_level):
+        if not isinstance(new_level, str):
+            raise TypeError("'level' must be a string.")
+        if new_level not in Employee._base_salaries:
+            raise ValueError(f"Invalid value '{new_level}' for 'level' attribute.")
+        if hasattr(self, '_level') and new_level == self.level:
+            raise ValueError(f"'{self.level}' is already the selected level.")
+        if hasattr(self, '_level') and Employee._base_salaries[new_level] < Employee._base_salaries[self.level]:
+            raise ValueError("Cannot change to lower level.")
+        print(f"'{self.name}' promoted to '{new_level}'.")
+        self.salary = Employee._base_salaries[new_level]
+        self._level = new_level
 
-# Try to access radius after deletion
-try:
-    print(my_circle.radius)
-except AttributeError as e:
-    print("Error:", e) # Error: 'Circle' object has no attribute '_radius'
+    @property
+    def salary(self):
+        return self._salary
+
+    @salary.setter
+    def salary(self, new_salary):
+        if not isinstance(new_salary, (int, float)):
+            raise TypeError("'salary' must be a number.")
+        if hasattr(self, '_level') and new_salary < Employee._base_salaries[self.level]:
+            raise ValueError(f'Salary must be higher than minimum salary ${Employee._base_salaries[self.level]}.')
+        self._salary = new_salary
+        print(f'Salary updated to ${self.salary}.')
+
+charlie_brown = Employee('Charlie Brown', 'trainee')
+print(charlie_brown)
+print(f'Base salary: ${charlie_brown.salary}')
+charlie_brown.level = 'senior'
